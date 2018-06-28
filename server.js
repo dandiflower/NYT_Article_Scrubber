381f3bed902b6d00ad698f28940f0a53de564450
Jessica Brush <jessicadawnbrush@gmail.com>
2018-06-27 21:22:07 -0700

    heroku

--- server.js
+++ server.js
@@ -34,7 +34,10 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
-
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Start the API server
 app.listen(PORT, function () {
